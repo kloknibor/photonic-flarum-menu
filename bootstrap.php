89494965e0676af3a06d3e2c4048afafc109b802
kloknibor <robinkolk@msn.com>
2016-04-29 21:22:58 +0200

    new try

--- bootstrap.php
+++ bootstrap.php
@@ -1,77 +1,76 @@
 <?php
 
 return function () {
-document.write("<section id=\"g-navigation\">");
-document.write("                <div class=\"g-container\">                        ");
-document.write("");
-document.write("    <div class=\"g-grid\">");
-document.write("                              ");
-document.write("");
-document.write("    <div class=\"g-block size-100\">");
-document.write("                                                <div class=\"g-content g-particle\">");
-document.write("                            ");
-document.write("");
-document.write("");
-document.write("");
-document.write("");
-document.write("");
-document.write("");
-document.write("");
-document.write("<nav class=\"g-main-nav\" role=\"navigation\"><ul class=\"g-toplevel\">");
-document.write("                                                                                        ");
-document.write("        <li class=\"g-menu-item g-menu-item-type-custom g-menu-item-37 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-37\">");
-document.write("            <a class=\"g-menu-item-container\" href=\"https:\/\/photonic3d.com\/\">");
-document.write("                                                                <span class=\"g-menu-item-content\">");
-document.write("                                    <span class=\"g-menu-item-title\">Home<\/span>");
-document.write("            ");
-document.write("                    <\/span>");
-document.write("                                                <\/a>");
-document.write("                                <\/li>");
-document.write("    ");
-document.write("                                                                            ");
-document.write("        <li class=\"g-menu-item g-menu-item-type-custom g-menu-item-16 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-16\">");
-document.write("            <a class=\"g-menu-item-container\" href=\"http:\/\/forum.photonic3d.com\/\">");
-document.write("                                                                <span class=\"g-menu-item-content\">");
-document.write("                                    <span class=\"g-menu-item-title\" style=\"font-weight: 500\">Forum<\/span>");
-document.write("            ");
-document.write("                    <\/span>");
-document.write("                                                <\/a>");
-document.write("                                <\/li>");
-document.write("    ");
-document.write("                                                                            ");
-document.write("        <li class=\"g-menu-item g-menu-item-type-custom g-menu-item-36 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-36\">");
-document.write("            <a class=\"g-menu-item-container\" href=\"https:\/\/photonic3d.com\/downloads\/\">");
-document.write("                                                                <span class=\"g-menu-item-content\">");
-document.write("                                    <span class=\"g-menu-item-title\">Downloads<\/span>");
-document.write("            ");
-document.write("                    <\/span>");
-document.write("                                                <\/a>");
-document.write("                                <\/li>");
-document.write("    ");
-document.write("                                                                            ");
-document.write("        <li class=\"g-menu-item g-menu-item-type-post_type g-menu-item-43 active g-standard   menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-41 current_page_item menu-item-43\">");
-document.write("            <a class=\"g-menu-item-container\" href=\"https:\/\/wiki.photonic3d.com\/\">");
-document.write("                                                                <span class=\"g-menu-item-content\">");
-document.write("                                    <span class=\"g-menu-item-title\">Docs<\/span>");
-document.write("            ");
-document.write("                    <\/span>");
-document.write("                                                <\/a>");
-document.write("                                <\/li>");
-document.write("    ");
-document.write("    ");
-document.write("    <\/ul>");
-document.write("    ");
-document.write("<\/nav>");
-document.write("            ");
-document.write("    <\/div>");
-document.write("                        ");
-document.write("    <\/div>");
-document.write("            ");
-document.write("    <\/div>");
-document.write("");
-document.write("");
-document.write("            <\/div>");
-document.write("        ");
-document.write("    <\/section>");
+    <section id="g-navigation">
+                <div class="g-container">                        
 
+    <div class="g-grid">
+                              
+
+    <div class="g-block size-100">
+                                                <div class="g-content g-particle">
+                            
+
+
+
+
+
+
+
+<nav class="g-main-nav" role="navigation"><ul class="g-toplevel">
+                                                                                        
+        <li class="g-menu-item g-menu-item-type-custom g-menu-item-37 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-37">
+            <a class="g-menu-item-container" href="https://photonic3d.com/">
+                                                                <span class="g-menu-item-content">
+                                    <span class="g-menu-item-title">Home</span>
+            
+                    </span>
+                                                </a>
+                                </li>
+    
+                                                                            
+        <li class="g-menu-item g-menu-item-type-custom g-menu-item-16 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-16">
+            <a class="g-menu-item-container" href="http://forum.photonic3d.com/">
+                                                                <span class="g-menu-item-content">
+                                    <span class="g-menu-item-title" style="font-weight: 500">Forum</span>
+            
+                    </span>
+                                                </a>
+                                </li>
+    
+                                                                            
+        <li class="g-menu-item g-menu-item-type-custom g-menu-item-36 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-36">
+            <a class="g-menu-item-container" href="https://photonic3d.com/downloads/">
+                                                                <span class="g-menu-item-content">
+                                    <span class="g-menu-item-title">Downloads</span>
+            
+                    </span>
+                                                </a>
+                                </li>
+    
+                                                                            
+        <li class="g-menu-item g-menu-item-type-post_type g-menu-item-43 active g-standard   menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-41 current_page_item menu-item-43">
+            <a class="g-menu-item-container" href="https://wiki.photonic3d.com/">
+                                                                <span class="g-menu-item-content">
+                                    <span class="g-menu-item-title">Docs</span>
+            
+                    </span>
+                                                </a>
+                                </li>
+    
+    
+    </ul>
+    
+</nav>
+            
+    </div>
+                        
+    </div>
+            
+    </div>
+
+
+            </div>
+        
+    </section>
 };
