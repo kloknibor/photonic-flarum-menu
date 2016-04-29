026493a1ab7fc1a7db94e00fa2606b84dcba4548
kloknibor <robinkolk@msn.com>
2016-04-29 23:19:20 +0200

    done

--- bootstrap.php
+++ bootstrap.php
@@ -1,76 +1,11 @@
 <?php
-
+    use Flarum\Event\ConfigureClientView;
+	
 return function () {
-    <section id="g-navigation">
-                <div class="g-container">                        
-
-    <div class="g-grid">
-                              
-
-    <div class="g-block size-100">
-                                                <div class="g-content g-particle">
-                            
-
-
-
-
-
-
-
-<nav class="g-main-nav" role="navigation"><ul class="g-toplevel">
-                                                                                        
-        <li class="g-menu-item g-menu-item-type-custom g-menu-item-37 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-37">
-            <a class="g-menu-item-container" href="https://photonic3d.com/">
-                                                                <span class="g-menu-item-content">
-                                    <span class="g-menu-item-title">Home</span>
-            
-                    </span>
-                                                </a>
-                                </li>
-    
-                                                                            
-        <li class="g-menu-item g-menu-item-type-custom g-menu-item-16 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-16">
-            <a class="g-menu-item-container" href="http://forum.photonic3d.com/">
-                                                                <span class="g-menu-item-content">
-                                    <span class="g-menu-item-title" style="font-weight: 500">Forum</span>
-            
-                    </span>
-                                                </a>
-                                </li>
-    
-                                                                            
-        <li class="g-menu-item g-menu-item-type-custom g-menu-item-36 g-standard   menu-item menu-item-type-custom menu-item-object-custom menu-item-36">
-            <a class="g-menu-item-container" href="https://photonic3d.com/downloads/">
-                                                                <span class="g-menu-item-content">
-                                    <span class="g-menu-item-title">Downloads</span>
-            
-                    </span>
-                                                </a>
-                                </li>
-    
-                                                                            
-        <li class="g-menu-item g-menu-item-type-post_type g-menu-item-43 active g-standard   menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-41 current_page_item menu-item-43">
-            <a class="g-menu-item-container" href="https://wiki.photonic3d.com/">
-                                                                <span class="g-menu-item-content">
-                                    <span class="g-menu-item-title">Docs</span>
-            
-                    </span>
-                                                </a>
-                                </li>
-    
-    
-    </ul>
-    
-</nav>
-            
-    </div>
-                        
-    </div>
-            
-    </div>
-
-
-            </div>
-        
-    </section>
+$events->listen(ConfigureClientView::class, function (ConfigureClientView $event) {
+    if ($event->isForum()) {
+        $event->addAssets(__DIR__.'/js/forum/dist/extension.js');
+        $event->addBootstrapper('acme/hello-world/main');
+    }
+});
 };
