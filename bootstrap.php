216c562db0be0a1d8953272459e7edc956a36dee
kloknibor <robinkolk@msn.com>
2016-04-30 00:19:52 +0200

    dev 001

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,10 @@
 <?php
-    use Flarum\Event\ConfigureClientView;
-	
-return function () {
-$events->listen(ConfigureClientView::class, function (ConfigureClientView $event) {
-    if ($event->isForum()) {
-        $event->addAssets(__DIR__.'/js/forum/dist/extension.js');
-        $event->addBootstrapper('acme/hello-world/main');
-    }
-});
+
+use Flarum\Event\PostWillBeSaved;
+use Illuminate\Contracts\Events\Dispatcher;
+
+return function (Dispatcher $events) {
+    $events->listen(PostWillBeSaved::class, function (PostWillBeSaved $event) {
+        $event->post->content = 'This is not what I wrote!';
+    });
 };
